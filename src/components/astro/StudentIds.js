919eb0d9201462bcb8f0c646bb3cf9915fcb40d9
itskelechi <149823828+itskelechi@users.noreply.github.com>
2025-04-10 20:31:45 -0400

    don't touch please

--- src/components/astro/StudentIds.js
+++ src/components/astro/StudentIds.js
@@ -6,12 +6,15 @@ const base = import.meta.env.BASE_URL == '/' ? '' : import.meta.env.BASE_URL;
 // Fetch the projects and build the tech counts
 const students = await getCollection('students');
 let studentIds = {};
+let studentNames = {};
 
 // Build tech counts from the projects
 students.forEach(student => {
   // Add the computed slug here
   student.data.slug = `${base}/students/${student.data.id}`;
-  student = studentIds[student.data.id];
+  studentIds[student.data.id] = student;
+  studentNames[student.data.name] = student;
+
 });
 
-export default { studentIds}
+export default {studentIds, studentNames};
