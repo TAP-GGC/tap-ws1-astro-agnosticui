aa5ec44d5a4f7c0583334fa3d7a6b251fa6be354
Thanh Tran <nhuthanhtran.bac@gmail.com>
2025-12-05 00:39:48 -0500

    project id and forum topic url mapping

---
+++ src/components/astro/pollTopicMap.js
@@ -0,0 +1,9 @@
+// Key: project.data.id 
+// Value: forumSlug/topicId from Discourse
+
+module.exports = {
+  "monster-makey": "teaching-sound-editing-with-audacity-and-makey-makey/125",
+  "ai-art-photos": "a-project-for-introducing-basic-ai-concepts-using-neural-style-transfer-as-a-teaching-platform/126",
+  "little-smash-guys": "little-smash-guys-a-game-that-showcases-chaos-and-strategy-in-a-free-for-all-battle/130"
+    
+}
